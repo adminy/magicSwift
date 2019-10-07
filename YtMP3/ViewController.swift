7cc7f720c7462200ae144cead6e90d3ccc8304c3
Marin <adminy@Marins-iMac.local>
2019-10-07 23:16:14 +0100

    Minor Changes

--- YtMP3/ViewController.swift
+++ YtMP3/ViewController.swift
@@ -7,15 +7,16 @@
 //
 
 import UIKit
-
+import mobileffmpeg
+//import HCYoutubeParser
 class ViewController: UIViewController {
     
     //@IBOutlet
-    var titleLabel: UITextView?
-    var urlTextfield: UITextField?
-    var progressBar: UIProgressView?
-    var statusLabel: UITextView?
-    var button: UIButton?
+    var titleLabel: UITextView!
+    var urlTextfield: UITextField!
+    var progressBar: UIProgressView!
+    var statusLabel: UITextView!
+    var button: UIButton!
     
     
     override func viewDidLoad() {
@@ -25,45 +26,57 @@ class ViewController: UIViewController {
         //Title Label
         titleLabel = UITextView(frame:
             CGRect(x: 20, y: 80, width: self.view.frame.size.width - 40, height: 40))
-        titleLabel?.text = "Youtube MP3 Downloader"
-        titleLabel?.font = UIFont(name: "Arial", size: 28)
-        self.view.addSubview(titleLabel!)
+        titleLabel.text = "Youtube MP3 Downloader"
+        titleLabel.font = UIFont(name: "Arial", size: 28)
+        self.view.addSubview(titleLabel)
     
         // TEXTBox
         urlTextfield = UITextField(frame:
                 CGRect(x: 20, y: self.view.frame.size.height * 0.3, width: self.view.frame.size.width - 40, height: 40))
-        urlTextfield?.placeholder = "Youtube URL Here ..."
-        self.view.addSubview(urlTextfield!)
+        urlTextfield.placeholder = "Youtube URL Here ..."
+        self.view.addSubview(urlTextfield)
         
         //ProgressBar
         progressBar = UIProgressView(frame:
             CGRect(x: 20, y: self.view.frame.size.height * 0.5, width: self.view.frame.size.width - 40, height: 40))
-        progressBar?.trackTintColor = UIColor.lightGray
-        progressBar?.tintColor = UIColor.blue
-        view.addSubview(progressBar!)
+        progressBar.trackTintColor = UIColor.lightGray
+        progressBar.tintColor = UIColor.blue
+        view.addSubview(progressBar)
         
         //Status Label
         statusLabel = UITextView(frame:
             CGRect(x: 20, y: self.view.frame.size.height * 0.52, width: self.view.frame.size.width - 40, height: 220))
-        statusLabel?.font = UIFont(name: "Arial", size: 12)
-        self.view.addSubview(statusLabel!)
+        statusLabel.font = UIFont(name: "Arial", size: 12)
+        self.view.addSubview(statusLabel)
         
         // Button
         button = UIButton(frame:
             CGRect(x: 80, y: self.view.frame.size.height * 0.75, width: self.view.frame.size.width - 160, height: 40 ))
-        button?.backgroundColor = .black
-        button?.setTitle("Give Me MP3", for: .normal)
-        button?.addTarget(self, action:#selector(self.buttonClicked), for: .touchUpInside)
-        self.view.addSubview(button!)
+        button.backgroundColor = .black
+        button.setTitle("Give Me MP3", for: .normal)
+        button.addTarget(self, action:#selector(self.buttonClicked), for: .touchUpInside)
+        self.view.addSubview(button)
         
        
+       
+        urlTextfield.text = "https://www.youtube.com/watch?v=GZjt_sA2eso"
         
     }
     
     @objc func buttonClicked() {
-        progressBar?.setProgress(1, animated: true)
+        progressBar.setProgress(1, animated: true)
         
-        statusLabel?.text = "Done ....\nsaid the wise one\n............................................................................................................................."
+        statusLabel.text = "Done ....\nsaid the wise one\n....................."
+        
+//        let video = HCYoutubeParser.h264videos(withYoutubeURL:  URL(string: urlTextfield.text!))?.first?.value
+// 
+//        
+//        statusLabel.text = video as? String
+        
+        
+//        MobileFFmpeg.execute("-i video.mkv -codec copy video.mp4")
+        
+        
         
 //        let alert = UIAlertController(title: "URL is This", message: urlTextfield?.text, preferredStyle: .alert)
 //        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in}))
